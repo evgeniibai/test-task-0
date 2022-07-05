4577a0d2a1c9cdec99a57419633093aca4d937ed
Evgenii Bai <evgenii.bai@decathlon.com>
2022-07-05 12:35:36 +0300

    init commit

---
+++ src/main/java/ru/deca/example/testtask0/TestTask0Application.java
@@ -0,0 +1,13 @@
+package ru.deca.example.testtask0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTask0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTask0Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/deca/example/testtask0/TestTask0ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.deca.example.testtask0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTask0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
