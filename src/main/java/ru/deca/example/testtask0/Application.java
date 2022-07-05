b761da3a1328cfd67c6ff28bf494b8208e04a8d5
Evgenii Bai <evgenii.bai@decathlon.com>
2022-07-05 12:39:25 +0300

    minor changes

--- src/main/java/ru/deca/example/testtask0/Application.java
+++ src/main/java/ru/deca/example/testtask0/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestTask0Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestTask0Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
