2bdfb7f881ee11afa191da9e9c1f46d18f01f0de
Eugene Bay <eugenebayz@gmail.com>
2022-07-19 22:30:19 +0300

    SecurityUser.java: accepts in the constructor model.User. SecurityUserDetailsService.java: removed static method, added SecurityUser.java constructor call. WebSecurityConfig.java: changed antMatchers to mvcMatchers.

--- src/main/java/ru/decathlon/example/test/task/security/SecurityUser.java
+++ src/main/java/ru/decathlon/example/test/task/security/SecurityUser.java
@@ -2,66 +2,53 @@ package ru.decathlon.example.test.task.security;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import ru.decathlon.example.test.task.model.Status;
 import ru.decathlon.example.test.task.model.User;
 
 import java.util.Collection;
-import java.util.Set;
 
-public record SecurityUser(String username,
-                           String password,
-                           Set<GrantedAuthority> authorities,
-                           boolean isAccountNonExpired,
-                           boolean isAccountNonLocked,
-                           boolean isCredentialsNonExpired,
-                           boolean isEnabled) implements UserDetails {
+public record SecurityUser(User user) implements UserDetails {
+
+    private static final String NOT_ACTIVE = Status.NOT_ACTIVE.name();
+    private static final String BANNED = Status.BANNED.name();
+    private static final String DELETED = Status.DELETED.name();
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
+        return user.getRole().getAuthorities();
     }
 
     @Override
     public String getPassword() {
-        return password;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return username;
+        return user.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return isAccountNonExpired;
+        String userStatus = user.getStatus().name();
+        return !userStatus.equals(NOT_ACTIVE);
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return isAccountNonLocked;
+        String userStatus = user.getStatus().name();
+        return !userStatus.equals(BANNED);
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return isCredentialsNonExpired;
+        String userStatus = user.getStatus().name();
+        return !userStatus.equals(NOT_ACTIVE);
     }
 
     @Override
     public boolean isEnabled() {
-        return isEnabled;
+        String userStatus = user.getStatus().name();
+        return !userStatus.equals(DELETED);
     }
-
-    public static UserDetails fromUser(User user) {
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                true,
-                true,
-                true,
-                true,
-                user.getRole().getAuthorities()
-        );
-    }
-
-    //TODO User on constructor
-    //TODO Translate logic into boolean methods
 }
